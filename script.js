10f2db718df26e626f22b3b45a349b13076fc81b
yankhomych <yan1999@meta.ua>
2019-08-25 22:47:25 +0300

    form for feedback

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-jQuery('document').ready(function(){
-    jQuery('.submit').on('click', function(){
-        alert('Thank You! Your message has been sent to Jan!')
+$("#sendMail").on("click", function(){
+    var email = $("#email").val().trim();
+    var name = $("#name").val().trim();
+    var message = $("#message").val().trim();
+
+    if(name == ""){
+        $("#errorMess").text("Vvedite email");
+        return false;
+    } else if(email == ""){
+        $("#errorMess").text("Vvedite name");
+        return false;
+    } else if(message == ""){
+        $("#errorMess").text("Vvedite message");
+        return false;
+    }
+
+    $("#errorMess").text("");
+
+    $.ajax({
+        url: 'mail.php',
+        type: 'POST',
+        cache : false,
+        data: { 'name': name, 'email': email, 'message': message },
+        dataType: 'html',
+        beforeSend: function() {
+            $("#sendMail").prop("disabled", true);
+        },
+        success: function(data) {
+            alert(data);
+            $("#sendMail").prop("disabled", false);
+        }
     });
 });
